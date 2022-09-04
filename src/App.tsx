78419ab96e5aa3af86935d306702108d51f75b5d
유용현 <yyh901@naver.com>
2022-09-05 01:57:21 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from "react";
-import TodoList from "./Render/TodoList";
-
+import TodoList from "./Render/Todo/TodoList";
+import Calendar from "./Render/Calendar/Calendar";
 import "./App.css";
+import Weather from "./Render/Weather/Weather";
 
 function App(): JSX.Element {
   return (
-    <>
+    <div className="todo-project">
       <TodoList />
-    </>
+      <div className="todo-project-side">
+        <Weather />
+        <Calendar />
+      </div>
+    </div>
   );
 }
 
